1e7fd514125d994d86d51f168cd099d2b68ae81e
tr288619 <133671736+tr288619@users.noreply.github.com>
2023-05-17 20:03:22 -0400

    Create git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Taylor Reigle
+ *        Date: May 17th, 2023
+ * Description: HW 1 CS 2400
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+  cout << "Hello Git!!" << endl
+    return 0;
+}// main
